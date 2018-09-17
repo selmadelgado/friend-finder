81831f9816ebd6e7dea538e9cd17dc6238ccc07b
Selma Delgado <selmadelgado@gmail.com>
2018-09-16 21:49:48 -0500

    App Structure Setup - Basic Form Working

---
+++ app/data/friend.js
@@ -0,0 +1,22 @@
+//array that holds all the dummy 'friends' 
+//initialize starting with 'dummy' data
+var friendList = [
+  {
+    name: 'Brooke Shields',
+    profilePic: 'https://goo.gl/images/demR1r',
+    scores: [
+      "4",
+      "5",
+      "5",
+      "2",
+      "4",
+      "5",
+      "4",
+      "4",
+      "3",
+      "4"
+    ]
+  }
+]
+
+module.exports = friendList;
