1f0ac13608440a071083dc9f086d6990cfbe312d
Selma Delgado <selmadelgado@gmail.com>
2018-09-16 23:04:26 -0500

    Added Additional Images - Ready to Deploy to Heroku

--- app/data/friend.js
+++ app/data/friend.js
@@ -5,18 +5,163 @@ var friendList = [
     name: 'Brooke Shields',
     profilePic: 'https://user-images.githubusercontent.com/15266541/45605216-e59f7880-ba00-11e8-9ffe-3c17c39c6f25.jpg',
     scores: [
-      "4",
-      "5",
-      "5",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1"
+    ]
+  },
+  {
+    name: 'Erik Estrada',
+    profilePic: 'https://user-images.githubusercontent.com/15266541/45605325-e5ec4380-ba01-11e8-9bb5-597e9c9728c3.jpg',
+    scores: [
       "2",
-      "4",
-      "5",
-      "4",
-      "4",
+      "2",
+      "2",
+      "2",
+      "2",
+      "2",
+      "2",
+      "2",
+      "2",
+      "2"
+    ]
+  },
+  {
+    name: 'Farah Fawcett',
+    profilePic: 'https://user-images.githubusercontent.com/15266541/45605359-1cc25980-ba02-11e8-8cfa-081388a59fc8.jpg',
+    scores: [
       "3",
+      "3",
+      "3",
+      "3",
+      "3",
+      "3",
+      "3",
+      "3",
+      "3",
+      "3"
+    ]
+  },
+  {
+    name: 'Heather Locklear',
+    profilePic: 'https://user-images.githubusercontent.com/15266541/45605377-3bc0eb80-ba02-11e8-8081-346d8e780fc1.jpg',
+    scores: [
+      "4",
+      "4",
+      "4",
+      "4",
+      "4",
+      "4",
+      "4",
+      "4",
+      "4",
       "4"
     ]
+  },
+  {
+    name: 'Hulk Hogan',
+    profilePic: 'https://user-images.githubusercontent.com/15266541/45605398-5bf0aa80-ba02-11e8-95b8-35fdcf744b29.jpg',
+    scores: [
+      "5",
+      "5",
+      "5",
+      "5",
+      "5",
+      "5",
+      "5",
+      "5",
+      "5",
+      "5"
+    ]
+  },
+  {
+    name: 'Jacklyn Smith',
+    profilePic: 'https://user-images.githubusercontent.com/15266541/45605446-a70abd80-ba02-11e8-8192-8e283d101d67.jpg',
+    scores: [
+      "6",
+      "6",
+      "6",
+      "6",
+      "6",
+      "6",
+      "6",
+      "6",
+      "6",
+      "6"
+    ]
+  },
+  {
+    name: 'Larry Hagman',
+    profilePic: 'https://user-images.githubusercontent.com/15266541/45605470-c73a7c80-ba02-11e8-9547-6fc96c6f8bb7.jpg',
+    scores: [
+      "7",
+      "7",
+      "7",
+      "7",
+      "7",
+      "7",
+      "7",
+      "7",
+      "7",
+      "7"
+    ]
+  },
+  {
+    name: 'Lynda Carter',
+    profilePic: 'https://user-images.githubusercontent.com/15266541/45605484-eafdc280-ba02-11e8-8c39-0510553dcc59.jpg',
+    scores: [
+      "8",
+      "8",
+      "8",
+      "8",
+      "8",
+      "8",
+      "8",
+      "8",
+      "8",
+      "8"
+    ]
+  },
+  {
+    name: 'Mr. T',
+    profilePic: 'https://user-images.githubusercontent.com/15266541/45605520-17b1da00-ba03-11e8-819e-2f02a35e1cdd.jpg',
+    scores: [
+      "9",
+      "9",
+      "9",
+      "9",
+      "9",
+      "9",
+      "9",
+      "9",
+      "9",
+      "9"
+    ]
+  },
+  {
+    name: 'Tom Selleck',
+    profilePic: 'https://user-images.githubusercontent.com/15266541/45605530-387a2f80-ba03-11e8-930c-f12dbd7e9a95.jpg',
+    scores: [
+      "10",
+      "10",
+      "10",
+      "10",
+      "10",
+      "10",
+      "10",
+      "10",
+      "10",
+      "10"
+    ]
   }
+
 ]
 
 module.exports = friendList;
